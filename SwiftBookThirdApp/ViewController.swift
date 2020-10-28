df39ebe7a8f1ff4f5951e668418895624dc5e8c3
Mikhail Moskvichev <mikhailmos22@yandex.ru>
2020-10-28 19:57:45 +0300

    make all fields and label programs

--- SwiftBookThirdApp/ViewController.swift
+++ SwiftBookThirdApp/ViewController.swift
@@ -13,16 +13,28 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var monthTextField: UITextField!
     
-    @IBOutlet weak var yearTextField: UITextField!
+    @IBOutlet weak var earTextField: UITextField!
 
     @IBOutlet weak var resultLable: UILabel!
     
     @IBAction func findDay(_ sender: UIButton) {
     
+        let calender = Calendar.current
+        var dateComponents = DateComponents()
+        dateComponents.day = Int(dateTextField.text!)
+        dateComponents.month = Int(monthTextField.text!)
+        dateComponents.year = Int(earTextField.text!)
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "EEEE"
+        
+        let date = calender.date(from: dateComponents)
+   
+        let weekday = dateFormatter.string(from: date!)
+        
+        resultLable.text = weekday
     }
-
-
-
-
+    
+    
 }
 
