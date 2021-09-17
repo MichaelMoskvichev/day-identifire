a1c48eed90d96948195ef8e9e1e70c9f5afe01e7
Mikhail Moskvichev <mikhailmos22@yandex.ru>
2021-09-17 13:50:45 +0300

    end

--- SwiftBookThirdApp/ViewController.swift
+++ SwiftBookThirdApp/ViewController.swift
@@ -15,27 +15,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var earTextField: UITextField!
 
-    @IBOutlet weak var resultLable: UILabel!
+    @IBOutlet weak var resultLabel: UILabel!
     
     @IBAction func findDay(_ sender: UIButton) {
         
         guard let day = dateTextField.text, let month = monthTextField.text, let year = earTextField.text else
         { return }
     
-        let calender = Calendar.current
+        let calendar = Calendar.current
         var dateComponents = DateComponents()
         dateComponents.day = Int(day)
         dateComponents.month = Int(month)
         dateComponents.year = Int(year)
         
         let dateFormatter = DateFormatter()
+        dateFormatter.locale = Locale(identifier: "ru_Ru")
         dateFormatter.dateFormat = "EEEE"
         
-        guard let date = calender.date(from: dateComponents) else { return }
+        guard let date = calendar.date(from: dateComponents) else { return }
    
         let weekday = dateFormatter.string(from: date)
+        let capitalizedWeekday = weekday.capitalized
         
-        resultLable.text = weekday
+        resultLabel.text = capitalizedWeekday
+        
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
